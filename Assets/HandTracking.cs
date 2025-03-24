5affce243453ae9da3da324f67d7f2f606c27d5e
LolloGiampy <lorenzo.giampietro68@gmail.com>
2025-03-24 23:32:51 +0100

    Added implementation for second hand

--- Assets/HandTracking.cs
+++ Assets/HandTracking.cs
@@ -6,7 +6,9 @@ public class HandTracking : MonoBehaviour
 {
     // Start is called before the first frame update
     public UDPReceive udpReceive;
-    public GameObject[] handPoints;
+    public GameObject[] leftHandPoints;  // Points for left hand
+    public GameObject[] rightHandPoints; // Points for right hand
+    
     void Start()
     {
         
@@ -16,35 +18,50 @@ public class HandTracking : MonoBehaviour
     void Update()
     {
         string data = udpReceive.data;
-        // print(data);
 
         if (data == null)
         {
             Debug.Log("Data is null");
         }
+        
         if(data.Length > 1)
         {
             data = data.Remove(0, 1);
             data = data.Remove(data.Length-1, 1);
-            print(data);
             string[] points = data.Split(',');
-            print(points[0]);
-
-        //0        1*3      2*3
-        //x1,y1,z1,x2,y2,z2,x3,y3,z3
-
-            for ( int i = 0; i<21; i++)
+            
+            if (points.Length < 2) {
+                Debug.Log("Not enough data");
+            }; // Not enough data
+            
+            // First value tells us how many hands we have
+            int handCount = int.Parse(points[0]);
+            
+            int currentIndex = 1;
+            
+            // Process each hand
+            for (int hand = 0; hand < handCount && hand < 2; hand++)
             {
-
-                float x = 7-float.Parse(points[i * 3])/100;
-                float y = float.Parse(points[i * 3 + 1]) / 100;
-                float z = float.Parse(points[i * 3 + 2]) / 100;
-
-                handPoints[i].transform.localPosition = new Vector3(x, y, z);
-
+                // Get hand type (0 for left, 1 for right)
+                int handType = int.Parse(points[currentIndex]);
+                currentIndex++;
+                
+                GameObject[] targetHandPoints = (handType == 1) ? rightHandPoints : leftHandPoints;
+                
+                // Process 21 landmarks for this hand
+                for (int i = 0; i < 21; i++)
+                {
+                    if (currentIndex + 2 >= points.Length) break; // Safety check
+                    
+                    float x = 7 - float.Parse(points[currentIndex]) / 100;
+                    float y = float.Parse(points[currentIndex + 1]) / 100;
+                    float z = float.Parse(points[currentIndex + 2]) / 100;
+                    
+                    targetHandPoints[i].transform.localPosition = new Vector3(x, y, z);
+                    
+                    currentIndex += 3; // Move to next point (x,y,z)
+                }
             }
-
-
         }
     }
 }
